6ee05ea422ea0d4239e12f7d9a8cdcf3b9a5aba7
mcbazi <marcus.carvalho@aiesec.net>
2015-08-10 11:25:28 -0300

    Comments for root

--- podio_bazi.rb
+++ podio_bazi.rb
@@ -9,7 +9,19 @@ require_relative 'tm/tm'
 
 # This is the root class of the PodioBAZI project. The PodioBAZI project creates standards procedures for different
 # areas of AIESEC in Brazil's Podio. It also automate procedures by controled data manipulation/duplication.
+#
+# @author Marcus Vinicius de Carvalho (marcus.carvalho@aiesec.net)
 
+# = This class initialize and start the batch script
+# == Priority of functional areas to work:
+# * 1. TM
+# * 2. oGIP
+# * 3. oGCDP
+# * 4. MKT
+# * 5. FIN
+# * 6. iGIP
+# * 7. iGCDP
+# * 8. PR/BD (Alumnus)
 class PodioBAZI
   def initialize
     data = File.read('senha').each_line()
@@ -34,6 +46,7 @@ class PodioBAZI
     #TODO BD/PR
   end
 
+  # Authenticate at Podio with robozinho credentials
   def authenticate
     Podio.setup(:api_key => @enum_robot[:api_key], :api_secret => @enum_robot[:api_secret])
     Podio.client.authenticate_with_credentials(@enum_robot[:username], @enum_robot[:password])
